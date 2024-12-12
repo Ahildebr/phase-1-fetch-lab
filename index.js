6ac8586a1b6796f56c51be25e1d4d0a29829975a
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2024-12-12 15:30:37 -0800

    Completes Lab

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+
+function fetchBooks(){
+  let books = []
+ return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((data) => {
+    renderBooks(data)
+  }) 
 }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +22,15 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+// function fetchBooks() {
+//   // To pass the tests, don't forget to return your fetch!
+//   fetch("https://anapioficeandfire.com/api/books")
+//   .then(response => {
+//     console.log(response)
+//     return response.json()
+//   })
+//   .then(data => {
+
+//   })
+// }
